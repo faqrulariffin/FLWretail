10afd26fdff735eb7ac19680d328c04ff6965974
Faqrul Ariffin <faqrulariffin@gmail.com>
2021-05-31 23:00:55 +0800

    v1.0 of checkShift script: - gets values of date - compare dateSubmitted value with date from sheet (dateArray) - return array of useremail which meets this condition

--- CheckShift.js
+++ CheckShift.js
@@ -1,5 +1,14 @@
-function checkShift() {
-    console.log("test");
-    console.log("2nd line test")
-    return;
+function checkShift(useremail, dateSubmitted) {
+    let scheduleSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Schedule");
+    let dateArray = scheduleSheet.getRange(2, 3, scheduleSheet.getLastRow()).getValues().flat();
+    let employeeArray = scheduleSheet.getRange(2, 2, scheduleSheet.getLastRow()).getValues().flat();
+    let sameShiftArray = [];
+
+    for(let i = 0; i < dateArray.length; i++) {
+        if (dateArray[i] == dateSubmitted) {
+            sameShiftArray.push(employeeArray[i]);
+        }
+    }
+
+    return sameShiftArray;
 }
